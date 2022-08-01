1bdc47e3bad884714ce85f9ec0c125c353ec0fb1
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-08-02 01:48:22 +0530

    Initial commit
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# PyCLI
+
+A CLI to run your server for WSGI and ASGI apps at a production level without much hassle! It is a one stop solution for your Python app
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use derive;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    name: String,
+    #[clap(short, long)]
+    index: i32,
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+        println!("Index {}!", args.index)
+    }
+}
